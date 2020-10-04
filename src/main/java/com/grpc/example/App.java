348af59a19128267b1ad2fe5c81de79c64dec051
Aride Chettali <achettali@paypal.com>
2020-10-04 18:59:21 +0530

    initial commit

---
+++ src/main/java/com/grpc/example/App.java
@@ -0,0 +1,13 @@
+package com.grpc.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
